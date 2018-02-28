a8273ba1a83c2475d2959c2ad179ddd160f79985
oscartorres098 <36300388+oscartorres098@users.noreply.github.com>
2018-02-28 08:50:45 -0500

    Update matriztriangular.py

--- matriztriangular.py
+++ matriztriangular.py
@@ -1,14 +1,20 @@
+def MeasureTime( v, t ):
+	sT = time.process_time( )
+	leer_matriz(v, t)
+	triangulo_superior(v, t)
+	eT = time.process_time( )
+	return float( eT - sT )
+
+
 def leer_matriz(v, t):
-	for(i=0; i<t; i++):
-		for(j=0; j<t; j++):
+	for i in range (t):
+		for j in range (t):
 			print ("Escriba el elemento %u de la columna %u: ", j+1, i+1)
 			v[i][j] = input()
-	for(i=0; i<t; i++):
-		for(j=0; j<t; j++):
+	for i in range (t):
+		for j in range (t):
 			printf(v[i][j])
-	print("\n");
-    }
-}
+	print("\n")
  
 def triangulo_superior(v, unsigned t):
 	x = 0
@@ -22,6 +28,5 @@ def triangulo_superior(v, unsigned t):
 max = 5
 print ("Cuantos elementos va a tener la matriz?: (maximo 5)");
 t = input()
-leer_matriz(v, t)
-triangulo_superior(v, t)
+
  
