1d4cbe01f224d959b5eb312830c02b65519643b4
oscartorres098 <36300388+oscartorres098@users.noreply.github.com>
2018-02-28 23:00:23 -0500

    Update punto3.R

--- punto3.R
+++ punto3.R
@@ -1,39 +1,31 @@
 
-rm(list=ls())
-Fx <- function(x) exp(-x) + x -2
-F1x <- function(x) 1-exp(-x)
-
-newton <- function(x) {
-for(i in 1:20) {
-x<-x-Fx(x)/F1x(x)
-if (Fx(x) == 0) break
-error<-abs(Fx(x)/F1x(x))
-cat("X=",x,"\t","E=",error,"\n")
-}
+secante <- function(f,x1,x2,E) {
+	pns1 <- x1
+	pns2 <- x2
+	while (abs(f(pns1)) > E) {
+		pn <- pns1 - (f(pns1)*(pns1 - pns2))/(f(pns1) - f(pns2))
+		pns2 <- pns1
+		pns1 <- pn
+	}
+	pns1
 }
 
+newton_generalizado <- function(f,x0,E) {
+	df <- function(x) {}
+	body(df) <- D(body(f),'x')
+	ddf <- function(x) {}
+	body(ddf) <- D(body(df),'x')
 
-rm(list=ls())
-Fx <- function(x) exp(-x) + x -2
-F1x <- function(x) 1-exp(-x)
-# Metodo de la Secante
-# Halla la raiz de Fx
-
-secante <- function(x0,x1) {
-x<-(Fx(x1)*x0-Fx(x0)*x1)/(Fx(x1)-Fx(x0))
-error <-1
-while (error > 1.e-4) {
-x0<-x1
-x1<-x
-x<-(Fx(x1)*x0-Fx(x0)*x1)/(Fx(x1)-Fx(x0))
-if (Fx(x) == 0) break
-error<-abs(Fx(x)/F1x(x))
-cat("X=",x,"\t","E=",error,"\n")
+	x <- x0
+	while (abs(f(x)) > E) {
+		x <- x - f(x)*df(x)/(ddf(x)^2 - f(x)*ddf(x))
+	}
+	x
 }
-}
-secante(0,3)
 
-x <- newton_g(f,-1.8,1e-5)
+f <- function (x) (log(x+2) - sin(x))
+
+x <- newton_generalizado(f,-1.8,1e-5)
 print(paste('x',x))
 
 x <- secante(f, -1.8, 0, 1e-7)
